27442616798ca47000f8abdfee5a94ae267ff31f
CBAmsterdam <info@iloki.nl>
2019-02-13 15:09:01 +0100

    more jenkins stuff

--- api/deploy/ovlookup_data/dc-import-trips.sh
+++ api/deploy/ovlookup_data/dc-import-trips.sh
@@ -10,7 +10,7 @@ export PYTHONPATH=/app/
 DIR=`dirname "$0"`
 
 dc() {
-	docker-compose -p ndw-${ENVIRONMENT} -f $DIR/docker-compose.yml $*
+	docker-compose -p ov-api-jobs-${ENVIRONMENT} -f $DIR/docker-compose.yml $*
 }
 
 trap 'dc kill ; dc down ; dc rm -f' EXIT
