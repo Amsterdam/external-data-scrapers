ff1c12a372a8991dfaea6b7cfc35fe1446626320
cuong <info@iloki.nl>
2019-04-10 16:34:30 +0200

    use new python image

--- api/deploy/archive/archive-tables.sh
+++ api/deploy/archive/archive-tables.sh
@@ -3,10 +3,10 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 
 # set env variables for pg_* tools
-export PGUSER=${DATABASE_USER:='externaldata'}
+export PGUSER=${DATABASE_WRITE_USER:='externaldata'}
 export PGDATABASE=${DATABASE_NAME:='externaldata'}
-export PGHOST=${DATABASE_HOST:='database'}
-export PGPASSWORD=${DATABASE_PASSWORD:='insecure'}
+export PGHOST=${DATABASE_WRITE_HOST_OVERRIDE:='database'}
+export PGPASSWORD=${DATABASE_WRITE_PASSWORD:='insecure'}
 export TENANT_NAME=${TENENT_NAME:='BGE000081 externaldata'}
 export TENANT_ID=${TENANT_ID:='12777ad76d604757a0499fb62d7ab4a9'}
 export OBJECTSTORE_USER=${OBJECTSTORE_USER:='externaldata'}
