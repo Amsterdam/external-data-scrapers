83834f80a6a562665137b434dbcc77cb7e87ed13
cuong <info@iloki.nl>
2019-04-17 14:13:43 +0200

    summary script for ov travel time

--- api/deploy/ovlookup_data/sum.sql
+++ api/deploy/ovlookup_data/sum.sql
@@ -1,56 +1,66 @@
-DROP TABLE IF EXISTS sum_ovkv6_speed;
-DROP TABLE IF EXISTS sum_time;
-
-create table sum_time (
+create table if not exists sum_time (
 	id serial,
+	setid integer,
 	start_time integer,
 	end_time integer,
 	description varchar(255),
 	PRIMARY KEY(id)
 );
 
-insert into sum_time(start_time, end_time, description) values(6, 9, 'ochtend spits');
-insert into sum_time(start_time, end_time, description) values(16, 18, 'avond spits');
-insert into sum_time(start_time, end_time, description) values(null, null, 'anders');
+-- 'upserts'
+insert into sum_time(setid, start_time, end_time, description) values(1, 6, 9, 'ochtend spits') ON CONFLICT DO NOTHING;
+insert into sum_time(setid, start_time, end_time, description) values(1, 16, 18, 'avond spits') ON CONFLICT DO NOTHING;
+insert into sum_time(setid, start_time, end_time, description) values(1, null, null, 'anders') ON CONFLICT DO NOTHING;
 
-
-
-create table sum_ovkv6_speed (
-	tid integer references sum_time(id),
-    day date not null,
+create table if not exists sum_ovkv6_speed (
+	id bigserial, -- for mapserver 
+	bucket integer references sum_time(id),
+    vehicle_date date not null,
 	dataownercode varchar(255) not null,
 	lineplanningnumber varchar(255) not null,
 	journeynumber integer not null,
+	geo_location geometry(LineString, 4326),
+	userstopcode varchar(255),
+	prev_userstopcode varchar(255),
 	sum_distance integer,
 	sum_time integer,
 	avg_speed float	
 );
 
+-- Enforce unique constraint on combination of a row. 
+-- We can abuse this to redo insertions of a partition multiple times and continue on constraint conflicts.
+-- (== it is safe to do multiple summaries on the same partition)
+CREATE unique INDEX IF NOT exists sum_kv6_day_idx  ON sum_ovkv6_speed (vehicle_date, bucket, dataownercode, lineplanningnumber, journeynumber, geo_location);
 
-
+insert into sum_ovkv6_speed
+	(vehicle_date, bucket, dataownercode, lineplanningnumber, journeynumber, geo_location, userstopcode, prev_userstopcode, sum_distance, sum_time, avg_speed)
 select
+	o.vehicle::date as grouped_day,
 	coalesce((
 		select id from sum_time t 
 		where t.start_time >= extract(hour from o.vehicle)::int
 		and t.end_time <= extract(hour from o.vehicle)::int
+		and setid = 1 -- can store different sets for different slices
 	), 3) as bucket,
-	o.vehicle::date as grouped_day,
 	o.dataownercode,
 	o.lineplanningnumber,
 	o.journeynumber,
-	o.geo_location,
-	o.prev_geo_location,
+	ST_MakeLine(ST_Transform(o.geo_location, 4326),
+				ST_Transform(o.prev_geo_location, 4326)),
 	o.userstopcode,
 	o.prev_userstopcode,
 	sum(o.distance) as sum_distance,
 	sum(o.time) as sum_time,
-	sum(o.distance) / sum(o.time) * 60*60/1000.0 as avg_speed
+	sum(o.distance) / sum(o.time) * 60*60/1000.0 as avg_speed -- km/hour
 from ov_ovkv6 o
 where 
 	o.geo_location is not null
 	and o.prev_geo_location is not null
 	and o.distance is not null
 	and o.time is not null
+	-- get start and end date of current week, will match with ov_kv6raw week partitions
+	and o.vehicle::date >= date_trunc('week', now()::date)
+	and o.vehicle::date < date_trunc('week', now()::date) + interval '1 week'
 group by 
 	grouped_day,
 	o.dataownercode,
@@ -60,6 +70,7 @@ group by
 	o.prev_geo_location,
 	o.userstopcode,
 	o.prev_userstopcode,	
-	bucket;
+	bucket
+	ON CONFLICT DO NOTHING;
 
 
