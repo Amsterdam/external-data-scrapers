a46a6a419964cebc3e6ce17ec01379296257efe4
Yahia <yahia.elsherbini@elements.nl>
2019-05-14 14:55:07 +0200

    Added sh script to recreate views

---
+++ import/deploy/import/drop_ndw_views.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Import ndw 
+
+set -u   # crash on missing env variables
+set -e   # stop on any error
+set -x   # print what we are doing
+
+export PYTHONPATH=/app/
+DIR=`dirname "$0"`
+
+dc() {
+	docker-compose -p ndw-dropviews-${ENVIRONMENT} -f $DIR/docker-compose.yml $*
+}
+
+trap 'dc kill ; dc down ; dc rm -f' EXIT
+
+dc rm -f
+dc pull
+dc build
+
+dc run --rm importer python data_sources/ndw/models.py --drop_views
+
+dc down -v
+
