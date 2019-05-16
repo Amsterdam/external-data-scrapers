163f50450226a975beca4e5b9458284f99d06bc6
Yahia <yahia.elsherbini@elements.nl>
2019-05-16 17:44:16 +0200

    Added summarize sh script for ovkv6

---
+++ api/deploy/ovlookup_data/dc-summarize-ov.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -u   # crash on missing env variables
+set -e   # stop on any error
+set -x   # print what we are doing
+
+export pythonpath=/app/
+dir=`dirname "$0"`
+
+dc() {
+	docker-compose -p ov-api-summarize-jobs${ENVIRONMENT} -f $dir/docker-compose.yml $*
+}
+
+trap 'dc kill ; dc down ; dc rm -f' exit
+
+dc rm -f
+dc pull
+dc build
+
+dc run --rm api psql -U $DATABASE_USER -h $DATABASE_HOST $DATABASE_NAME -v ON_ERROR_STOP=1 -f /deploy/ovlookup_data/sum.sql
+
+dc down -v
