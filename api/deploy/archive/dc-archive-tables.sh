ccc3152ac675db9b74fa47545b513f0f3ed28f53
Yahia <yehiatharwat@gmail.com>
2019-10-02 10:40:41 +0200

    Added separate script for achiving tables

---
+++ api/deploy/archive/dc-archive-tables.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Import ndw 
+
+set -u   # crash on missing env variables
+set -e   # stop on any error
+set -x   # print what we are doing
+
+export PYTHONPATH=/app/
+DIR=`dirname "$0"`
+
+dc() {
+	docker-compose -p archive-job-${ENVIRONMENT} -f $DIR/docker-compose.yml $*
+}
+
+trap 'dc kill ; dc down ; dc rm -f' EXIT
+
+dc rm -f
+dc pull
+dc build
+
+dc run --rm api /deploy/archive/archive-tables.sh
+
+dc down -v
+
