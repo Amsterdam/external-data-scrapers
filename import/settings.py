26858059217078f48a5c2eda08607d83432a0ff2
Yahia <yehiatharwat@gmail.com>
2019-10-30 09:56:48 +0100

    Added shapefile_name constant in the settings as temporary solution whenever the shapefile name changes on the server

--- import/settings.py
+++ import/settings.py
@@ -27,3 +27,8 @@ OBJECTSTORE_CONF = dict(
     PASSWORD=os.getenv('OS_PASSWORD'),
     REGION_NAME='NL',
 )
+
+# The name of the shapefile changes every now and then.
+# As a temporary solution, this constant is used and should be changed by developers -
+# whenever the name on the server changes. Later a regex implementation would be nicer.
+SHAPEFILE_NAME = 'WGS84-1'
